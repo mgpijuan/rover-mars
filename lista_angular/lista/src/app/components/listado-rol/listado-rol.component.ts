ad352a3c46f538cc4d362b1a6851e7aaac94ff3e
mgpijuan <mgpijuan@gmail.com>
2021-03-17 11:11:52 +0100

    primera version alfa

--- lista_angular/lista/src/app/components/listado-rol/listado-rol.component.ts
+++ lista_angular/lista/src/app/components/listado-rol/listado-rol.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
 
 @Component({
-  selector: 'app-listado-rol',
-  templateUrl: './listado-rol.component.html',
-  styleUrls: ['./listado-rol.component.css']
+selector: 'app-listado-rol',
+templateUrl: './listado-rol.component.html',
+styleUrls: ['./listado-rol.component.css']
 })
 export class ListadoRolComponent implements OnInit {
 
-  constructor() { }
+// eventoHijoMensaje
+@Output() rol = new EventEmitter()
+selectedItem = 'Jugador'
+rolesArray = [
+'Jugador', 'Árbitro', 'Entrenador', 'Otros'
+]
 
-  ngOnInit(): void {
-  }
+
+constructor() { }
+
+ngOnInit(): void {
+
+console.log('THIS.SELECTED iTEM', this.selectedItem)
+this.rol.emit(this.selectedItem);
+}
+
+onChange(ev:string){
+console.log('cambio del hijo', ev);
+this.rol.emit(ev);
+}
+
+
 
 }
